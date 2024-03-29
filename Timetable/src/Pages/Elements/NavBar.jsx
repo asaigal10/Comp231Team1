be405f0df41518cf1403e14612805b6b096cd15b
Kris Oud <oudster@gmail.com>
2024-03-29 16:09:44 -0400

    Homepage
    Styled basic of homepage
     *Updated navbar to highlight current day
     *Calaculated week within year
     *Added custom favicon

---
+++ Timetable/src/Pages/Elements/NavBar.jsx
@@ -0,0 +1,13 @@
+export default function NavBar({currentDayOfWeek}) {
+    return <div id="Navbar" className="flex flex-row justify-center w-screen max-w-6xl m-auto my-4">
+        <p className="mx-10 text-white hover:text-orange-500 flex-1 justify-items-start">Home</p>
+        <p className={"mx-2 text-white hover:text-orange-500 " + (currentDayOfWeek == 1 ? "underline font-bold" : "")}>Monday</p>
+        <p className={"mx-2 text-white hover:text-orange-500 " + (currentDayOfWeek == 2 ? "underline font-bold" : "")}>Tuesday</p>
+        <p className={"mx-2 text-white hover:text-orange-500 " + (currentDayOfWeek == 3 ? "underline font-bold" : "")}>Wednesday</p>
+        <p className={"mx-2 text-white hover:text-orange-500 " + (currentDayOfWeek == 4 ? "underline font-bold" : "")}>Thursday</p>
+        <p className={"mx-2 text-white hover:text-orange-500 " + (currentDayOfWeek == 5 ? "underline font-bold" : "")}>Friday</p>
+        <p className={"mx-2 text-gray-400 hover:text-orange-500 " + (currentDayOfWeek == 6 ? "underline font-bold" : "")}>Saturday</p>
+        <p className={"mx-2 text-gray-400 hover:text-orange-500 " + (currentDayOfWeek == 0 ? "underline font-bold" : "")}>Sunday</p>
+        <p className="mx-10 text-white hover:text-orange-500 flex-1 justify-self-end text-right">Settings</p>
+    </div>
+}
--- Timetable/src/main.jsx
+++ Timetable/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import App from './App.jsx'
-import ErrorPage from './error-page.jsx'
+import ErrorPage from './Pages/error-page.jsx'
 import './index.css'
 
 const router = createBrowserRouter([
