b0bfa84e81bbd9a655915ef54cb0e393e50464aa
Kris Oud <oudster@gmail.com>
2024-04-03 22:16:55 -0400

    Created modal
    *Added modal element for confirming choices
    *Added demo code in settings and notation at top of modal element

--- Timetable/src/Pages/Settings.jsx
+++ Timetable/src/Pages/Settings.jsx
@@ -1,5 +1,22 @@
+import { useState, useEffect } from "react";
+import Modal from "./Elements/Modal";
+
 export default function Settings() {
+
+    const [showModal, setShowModal] = useState(false)
+    const [confirmedChoice, setConfirmedChoice] = useState(null)
+
+    useEffect(() => {
+            setShowModal(false)
+    }, [confirmedChoice])
+
     return <div>
         <p>Setting!</p>
+        {/* Modal Demo */}
+        {showModal ? <Modal setConfirmedChoice={setConfirmedChoice} /> : <></>}
+       
+        <p>{"Modal Choice: " + (confirmedChoice ? "True" : "False")}</p>
+        <button className="p-5 m-5 bg-yellow-200 border-2 border-black rounded-md" onClick={() => {setShowModal(true)}}>Show Modal</button>
+        {/* End Modal Demo */}
     </div>
 }
